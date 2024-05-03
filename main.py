96e4206b82a1d7588fbc06fe3ce359c0096d96ee
aLEKS-e3 <chebukin404@gmail.com>
2024-05-03 16:28:26 +0300

    Create a new func to handle an experience oriented scraping

--- main.py
+++ main.py
@@ -2,11 +2,28 @@ from parse import DouVacancyScraper
 from storage import csv_writer
 
 
-def launch_scrape_analysis() -> None:
+PYTHON_URL = "https://jobs.dou.ua/vacancies/?category=Python"
+EXP_RANGES = {
+    "0-1": ("python_0-1", "&exp=0-1",),
+    "1-3": ("python_1-3", "&exp=1-3",),
+    "3-5": ("python_3-5", "&exp=3-5",),
+    "5+": ("python_5+", "&exp=5plus",)
+}
+
+
+def scrape_general_vacancy_data(url: str) -> None:
     scraper = DouVacancyScraper()
-    vacancies = scraper.scrape_all_vacancies()
+    vacancies = scraper.scrape_all_vacancies(url)
     csv_writer("python", vacancies)
 
 
-if __name__ == "__main__":
-    launch_scrape_analysis()
+def scrape_vacancy_data_by_experience(range: str) -> None:
+    scraper = DouVacancyScraper()
+
+    filename, url = EXP_RANGES.get(range)
+
+    if filename and url:
+        vacancies = scraper.scrape_all_vacancies(PYTHON_URL + url)
+        csv_writer(filename, vacancies)
+    else:
+        raise ValueError("Invalid range")
